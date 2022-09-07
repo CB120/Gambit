c37451630c062f2fc13c396ffd6c18004c5c10ec
BaxterDulieu <bdulieu@finessdata.com>
2022-09-07 20:00:48 +1000

    New Map Screen Logic

---
+++ Assets/Scripts/MapScript/MapRegion.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapRegion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    public Button button;
+
+    [Header("Details")]
+    public string mapName;
+    public int unitAmount;
+    public int enemyAmount;
+    public bool isUnlocked;
+    void Start()
+    {
+        
+    }
+
+}
