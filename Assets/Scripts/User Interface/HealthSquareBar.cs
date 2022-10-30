9af34f82c66a78526f98c310efb27fc4c37c9c5a
DelcanProbably <decbot100@gmail.com>
2022-10-31 10:16:28 +1100

    Added HealthSquareBar to UnitInfoCanvas

--- Assets/Scripts/User Interface/HealthSquareBar.cs
+++ Assets/Scripts/User Interface/HealthSquareBar.cs
@@ -7,6 +7,12 @@ public class HealthSquareBar : MonoBehaviour
 
     [SerializeField] GameObject healthSquarePrefab;
 
+    // Most unit health values should be even so that everything lines up with full squares
+    // In some contexts this doesn't matter as much, for example damage points shown on the unit info screen.
+    // In these cases we can turn this bool on to not spit a warning when there is an odd health value.
+    // This is not fully tested - you may need to flip the grid layout group and avoid using SetHealth.
+    [SerializeField] bool experimental_expectOddMaxHealth = false; 
+
     List<HealthSquare> healthSquares = new List<HealthSquare>();
 
     public void Initialise(int maxHealth) {
@@ -15,12 +21,19 @@ public class HealthSquareBar : MonoBehaviour
         }
         healthSquares.Clear();  
 
+        // Creates a health square for every even health square.
         for (int i = 0; i < maxHealth / 2; i++) {
-            GameObject newObject = Instantiate(healthSquarePrefab, transform);
-            HealthSquare newSquare = newObject.GetComponent<HealthSquare>();
-            newSquare.Setup();
-            newSquare.SetAmount(2);
-            healthSquares.Add(newSquare);
+            CreateNewHealthSquare();
+        }
+
+        if (maxHealth % 2 != 0) {
+            if (experimental_expectOddMaxHealth) {
+                // create last health square
+                CreateNewHealthSquare().SetAmount(1); // this is some gnarly shorthand lmao
+            } else {
+                // spit warning
+                Debug.LogWarning("HealthSquareBar initialised with odd max health. Results might not be as expected (enable expectOddMaxHealth in the inspector if you want to create a half square anyway).", this);
+            }
         }
     }
     // Redundancy to make it easy to integrate with existing code
@@ -42,21 +55,14 @@ public class HealthSquareBar : MonoBehaviour
             if (health < 0) health = 0;
         }
 
-        // int i = 0;
-        // while (health > 2) {
-        //     if (healthSquares.Count < i) {
-        //         healthSquares[i].SetAmount(2);
-        //     } else {
-        //         Debug.LogWarning("Health bar has less squares than required. You probably need to call the Initialise() function somewhere");
-        //     }
-        //     health -= 2;
-        //     i++;
-        // }
+    }
 
-        // if (healthSquares.Count < i) {
-        //     healthSquares[i].SetAmount(health);
-        // } else {
-        //     Debug.LogWarning("Health bar missing last square!");
-        // }
+    public HealthSquare CreateNewHealthSquare() {
+        GameObject newObject = Instantiate(healthSquarePrefab, transform);
+        HealthSquare newSquare = newObject.GetComponent<HealthSquare>();
+        newSquare.Setup();
+        newSquare.SetAmount(2);
+        healthSquares.Add(newSquare);
+        return newSquare;
     }
 }
