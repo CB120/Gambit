adda4d7a5a95a3abeaa4106a7a5d11b15319dc80
ItsFitzy <79495553+ItsFitzy@users.noreply.github.com>
2022-11-29 14:34:50 +1100

    Progress Save, Keyboard-Camera input's broken

--- Assets/Scripts/Game Systems/Camera System/NewCameraMovement.cs
+++ Assets/Scripts/Game Systems/Camera System/NewCameraMovement.cs
@@ -21,6 +21,9 @@ public class NewCameraMovement : MonoBehaviour
 
     [SerializeField] float lerpSpeed = 0.5f;
 
+    [SerializeField] float touchscreenPinchMultiplier = 0.5f;
+    [SerializeField] float dragYMinimumThreshold = 60f;
+
     // 0 is default, 1, 2, 3 are 90 degree increments rotating clockwise across the map.
     int rotationPosition = 0; //used for the rotation calculation of rotationPivot (and can go negative)
     [HideInInspector] public int rotationPositionNormalised = 0; //used for the rotation of the health bars (limited between 0-3, wraps)
@@ -29,6 +32,9 @@ public class NewCameraMovement : MonoBehaviour
 
     float targetCameraSize;
 
+    float oldPinchDistance = -1f; //-1 is a sentinal value
+    bool pinchInProgress = false;
+
     private void Awake() {
         Instance = this;
     }
@@ -36,40 +42,106 @@ public class NewCameraMovement : MonoBehaviour
     void Start() {
         panPosition = transform.position;
         targetCameraSize = Camera.main.fieldOfView;
-    }
+    } 
 
     void Update () {
         if (UIManager.isPaused) return; //GUARD to prevent camera movement when the game is paused
 
+        UpdateKeyboardRotation();
+
+        //GUARD to prevent the Player inputting camera panning during the AI's turn, created very hitchy camera movement
+        if (!ParticipantManager.IsCurrentParticipantType(ParticipantType.AI)) TranslateCameraKeyboard();
+
+        //UpdatePinchZoom() shouldn't call anything if the device doesn't have a touchscreen, but just in case
+        if (SystemInfo.deviceType == DeviceType.Handheld) UpdatePinchZoom();
+        
+        UpdateScrollZoom();
+
+        Vector3 targetPanPosition = UpdateCameraPosition();
+
+        UpdateCameraRotation();
+
+        UpdateCameraFOV();
+
+        // Handle mouse input after sexy lerped stuff cause the mouse feels weird with that cringe shit
+        if (Input.GetKey(KeyCode.Mouse0) && Input.touchCount < 2 && !pinchInProgress) UpdatePositionDrag(targetPanPosition);
+    }
+
+    void UpdateKeyboardRotation()
+    {
         if (Input.GetKeyDown(KeyCode.Q)) RotateSnapped(true);
         if (Input.GetKeyDown(KeyCode.E)) RotateSnapped(false);
+    }
 
-        Vector2 keyboardPanInput = new Vector2 (Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    void UpdatePinchZoom()
+    {
+        if (Input.touchCount >= 2) //adapted from http://shorturl.at/deSV3
+        {
+            Vector2 touch0, touch1;
+            touch0 = Input.GetTouch(0).position;
+            touch1 = Input.GetTouch(1).position;
+            float distance = Vector2.Distance(touch0, touch1);
 
-        TranslateCameraKeyboard(keyboardPanInput * Time.deltaTime);
+            if (oldPinchDistance >= 0f)
+            {
+                float delta = oldPinchDistance - distance;
+                AdjustZoom(delta * touchscreenPinchMultiplier);
+            }
+
+            oldPinchDistance = distance;
+            pinchInProgress = true;
+        }
+        else
+        {
+            oldPinchDistance = -1f;
+        }
+
+        //Had issues where if the Player released one finger but not the other, you'd get a weird camera snap-drag. This is to avoid that.
+        if (Input.touchCount == 0) pinchInProgress = false;
+    }
+
+    void UpdateScrollZoom()
+    {
         AdjustZoom(-Input.GetAxis("Mouse ScrollWheel"));
+    }
 
+    Vector3 UpdateCameraPosition()
+    {
         Vector3 targetPanPosition = panPosition;
         targetPanPosition.x = Mathf.Clamp(targetPanPosition.x, -maxPan, maxPan);
         targetPanPosition.z = Mathf.Clamp(targetPanPosition.z, -maxPan, maxPan);
         transform.position = Vector3.Lerp(transform.position, targetPanPosition, lerpSpeed * Time.deltaTime);
-
-        Quaternion targetRotation = Quaternion.Euler(Vector3.up * rotationPosition * 90);
-        rotationPivot.rotation = Quaternion.Lerp(rotationPivot.rotation, targetRotation, rotationSpeed * Time.deltaTime);
-
-        Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, targetCameraSize, lerpSpeed * Time.deltaTime);
-
-        // Handle mouse input after sexy lerped stuff cause the mouse feels weird with that cringe shit
-        if (Input.GetKey(KeyCode.Mouse0)) {
-            Vector2 mouseInput = new Vector2(-Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y")) * mousePanSpeed;
-            TranslateCameraMouse(mouseInput);
-            transform.position = targetPanPosition;
-        }
+        return targetPanPosition;
     }
 
-    void TranslateCameraKeyboard(Vector2 movement) {
-        if (ParticipantManager.IsCurrentParticipantType(ParticipantType.AI)) return; //GUARD to prevent the Player inputting camera panning during the AI's turn, created very hitchy camera movement
+    void UpdateCameraRotation()
+    {
+        Quaternion targetRotation = Quaternion.Euler(Vector3.up * rotationPosition * 90);
+        rotationPivot.rotation = Quaternion.Lerp(rotationPivot.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
 
+    void UpdateCameraFOV()
+    {
+        Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, targetCameraSize, lerpSpeed * Time.deltaTime);
+    }
+
+    void UpdatePositionDrag(Vector3 targetPanPosition)
+    {
+        Vector2 mouseInput = new Vector2(-Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y")) * mousePanSpeed;
+        if (Input.touchCount == 0)
+        {
+            if (!ParticipantManager.IsCurrentParticipantType(ParticipantType.AI)) TranslateCameraMouse(mouseInput);
+        }
+        else
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            if (mousePosition.y >= dragYMinimumThreshold && !ParticipantManager.IsCurrentParticipantType(ParticipantType.AI)) TranslateCameraMouse(mouseInput);
+        }
+        transform.position = targetPanPosition;
+    }
+
+    void TranslateCameraKeyboard() {
+        Vector2 movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         movement *= panSpeed;
         Vector3 translation = new Vector3(movement.y + movement.x, 0, movement.y - movement.x);
         panPosition += rotationPivot.TransformVector(translation);
@@ -78,8 +150,6 @@ public class NewCameraMovement : MonoBehaviour
     }
 
     void TranslateCameraMouse(Vector2 movement) { //Adapted from TranslateCameraKeyboard but with extra calculations to account for mouse-style movement
-        if (ParticipantManager.IsCurrentParticipantType(ParticipantType.AI)) return; //GUARD to prevent the Player inputting camera panning during the AI's turn, created very hitchy camera movement
-
         movement /= Screen.height;
         movement *= Camera.main.fieldOfView;
         movement *= 100; //to allow bigger numbers for mousePanSpeed
